447a8b0d3dac7a997e53125fc01880a14b956965
Rhyzen16 <102942367+Rhyzen16@users.noreply.github.com>
2023-01-14 15:05:16 +0800

    Add files via upload

---
+++ login2.js
@@ -0,0 +1,16 @@
+function validate()
+{
+var username=document.getElementById("username").value;
+var password=document.getElementById("password").value;
+if(username=="Admin1@rhyzen16"&& password=="RspyCMDS")
+{
+    alert("login succesfully");
+    window.open("https://zoro.to/watch/farming-life-in-another-world-18284?ep=97013");
+}
+else
+{
+    alert("login failed");
+}
+
+
+}
