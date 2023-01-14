44eb4b8850f2f89abe899b12f9e5a5f887b4e2f8
Rhyzen16 <102942367+Rhyzen16@users.noreply.github.com>
2023-01-14 21:32:25 +0800

    Update login2.js

--- login2.js
+++ login2.js
@@ -5,7 +5,7 @@ var password=document.getElementById("password").value;
 if(username=="Admin1@rhyzen16"&& password=="RspyCMDS")
 {
     alert("login succesfully");
-    window.open("https://zoro.to/watch/farming-life-in-another-world-18284?ep=97013");
+    window.location.assign("cmf1.html");
 }
 else
 {
