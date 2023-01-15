51bbd9abc4996b8998d985e2ac525d0ef8badeb4
Rhyzen16 <102942367+Rhyzen16@users.noreply.github.com>
2023-01-15 08:48:15 +0800

    Update login1.js

--- login1.js
+++ login1.js
@@ -4,7 +4,7 @@ var username=document.getElementById("username").value;
 var password=document.getElementById("password").value;
 if(username=="Admin1@rhyzen16"&& password=="RspyCMDS")
 {
-    window.open("cmf1.html");
+    window.open("login2.html");
     alert("login succesfully");
 }
 else
