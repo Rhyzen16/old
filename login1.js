b4c5225e2dba660e0647a78fc7a0f370d64ecfbb
Rhyzen16 <102942367+Rhyzen16@users.noreply.github.com>
2023-01-16 20:52:29 +0800

    Update login1.js

--- login1.js
+++ login1.js
@@ -5,7 +5,7 @@ var password=document.getElementById("password").value;
 if(username=="Admin1@rhyzen16"&& password=="RspyCMDS")
 {
     alert("login succesfully");
-    window.open("https://bobbyhadz.com/blog/javascript-window-location-href-is-not-a-function");
+    window.open("cmf2");
 }
 else
 {
