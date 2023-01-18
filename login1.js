78a1b0daa72fc544433036a4abbc4f24130f7c9a
Rhyzen16 <102942367+Rhyzen16@users.noreply.github.com>
2023-01-18 17:58:52 +0800

    Update login1.js

--- login1.js
+++ login1.js
@@ -5,8 +5,7 @@ var password=document.getElementById("password").value;
 if(username=="Admin1@rhyzen16"&& password=="RspyCMDS")
 {
     alert("login succesfully");
-    window.location.assign('https://developer.mozilla.org/en-US/docs/Web/API/Location/reload');
-    window.assign("");
+    window.open('https://developer.mozilla.org/en-US/docs/Web/API/Location/reload');
 }
 else
 {
